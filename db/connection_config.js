f6e241321ac1bcef29f2c17cb206895cccd4a2c6
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-09-24 16:45:22 -0300

    Move connection config to own file

---
+++ db/connection_config.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+
+module.exports = {
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE,
+};
