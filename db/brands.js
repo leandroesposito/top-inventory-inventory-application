ba58a2c344c8ae17e8113ad52a44ba7d2aa6473d
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-09-24 20:00:44 -0300

    Create brands db CRUD

---
+++ db/brands.js
@@ -0,0 +1,48 @@
+const { runQuery, runGetQuery } = require("./queries");
+
+async function createBrand({ name, country, founded_year, description }) {
+  const query =
+    "INSERT INTO brands (name, country, founded_year, description) VALUES ( $1 , $2 , $3 , $4 )";
+  const params = [name, country, founded_year, description];
+
+  await runQuery(query, params);
+}
+
+async function getAllBrands() {
+  const query =
+    "SELECT (id, name, country, founded_year, description) FROM brands";
+
+  return await runGetQuery(query);
+}
+
+async function getBrandById(id) {
+  const query = "SELECT * FROM brands WHERE id = $1";
+  const params = [id];
+
+  const brands = await runGetQuery(query, params);
+
+  return brands[0];
+}
+
+async function updateBrand(id, { name, country, founded_year, description }) {
+  const query =
+    "UPDATE brands SET name = $1, country = $2, founded_year = $3, description = $4 WHERE id = $5";
+  const params = [name, country, founded_year, description, id];
+
+  await runQuery(query, params);
+}
+
+async function deleteBrand(id) {
+  const query = "DELETE FROM brands WHERE id = $1";
+  const params = [id];
+
+  await runQuery(query, params);
+}
+
+module.exports = {
+  createBrand,
+  getAllBrands,
+  getBrandById,
+  updateBrand,
+  deleteBrand,
+};
