283be4ab3e0a0b771ad02e7e4d5026fde6bf915b
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-09-27 20:41:32 -0300

    Complete car controller

--- controllers/cars.js
+++ controllers/cars.js
@@ -1,81 +1,99 @@
-const { body, validationResults } = require("express-validator");
+const { validationResult } = require("express-validator");
+const specsDB = require("../db/car_specifications");
+const carDB = require("../db/cars");
 const {
-  getCarSpecsById,
-  getCarSpecsByCarId,
-} = require("../db/car_specifications");
-const { getAllCars, getCarById } = require("../db/cars");
-
-const ERRORS = {
-  INT: "must be integer",
-  LENGTH: (maxLength) =>
-    `must be at least 1 character and at most ${maxLength} characters`,
-  MAX: (max) => `must ve less tha ${max}`,
-  DECIMAL: (digits) => `must have ${digits} digits or none`,
-};
-
-function toCapitalized(string) {
-  return `${string.slice(0, 1).toUpperCase()}${string.slice(1).toLowerCase()}`;
-}
-
-function createIntChain(field, max = Number.MAX_SAFE_INTEGER) {
-  const casedField = toCapitalized(field);
-  return body(field)
-    .isInt()
-    .withMessage(`${casedField} ${ERRORS.INT}`)
-    .isInt({ min: 0, max: max })
-    .withMessage(`${casedField} ${ERRORS.MAX(max)}`);
-}
-
-function createTextChain(field, maxLength) {
-  const casedField = toCapitalized(field);
-  return body(field)
-    .trim()
-    .isLength({ min: 1, max: maxLength })
-    .withMessage(`${casedField} ${ERRORS.LENGTH(maxLength)}`);
-}
-
-function createDecimalChain(field, max, digits) {
-  const casedField = toCapitalized(field);
-  return body(field)
-    .isFloat({ lt: max })
-    .withMessage(`${casedField} ${ERRORS.MAX(max)}`)
-    .isDecimal({ force_decimal: false, decimal_digits: digits })
-    .withMessage(`${casedField} ${ERRORS.DECIMAL(digits)}`);
-}
+  createIntChain,
+  createTextChain,
+  createDecimalChain,
+} = require("./validators");
 
 const carValidations = [
-  createIntChain("category_id"),
-  createIntChain("brand_id"),
+  createIntChain("category-id"),
+  createIntChain("brand-id"),
   createTextChain("name", 100),
-  createIntChain("model_year"),
+  createIntChain("model-year"),
   createDecimalChain("price", 1e10, 2),
   createIntChain("horsepower"),
   createIntChain("torque"),
-  createIntChain("top_speed"),
-  createDecimalChain("acceleration_0_60", 1e4, 2),
+  createIntChain("top-speed"),
+  createDecimalChain("acceleration-0-60", 1e4, 2),
   createTextChain("transmission", 50),
   createTextChain("drivetrain", 50),
-  createTextChain("fuel_type", 50),
-  createTextChain("engine_size", 50),
+  createTextChain("fuel-type", 50),
+  createTextChain("engine-size", 50),
   createIntChain("seats"),
   createTextChain("color", 50),
 ];
 
 async function carsGet(req, res) {
-  const cars = await getAllCars();
+  const cars = await carDB.getAllCars();
 
-  res.status(200).render("cars", { title: "cars", cars });
+  res.status(200).render("cars.ejs", { title: "Cars", cars });
 }
 
-async function carGet(req, res) {
-  const { id } = req.params;
+const carGet = [
+  createIntChain("id"),
+  async function carGet(req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res
+        .status(400)
+        .render("cars.ejs", { title: "Cars", errors: errors.array() });
+    }
 
-  const car = await getCarById(id);
-  const specs = await getCarSpecsByCarId(id);
+    const { id } = req.params;
 
-  res.status(200).render("car", { title: car.name, car, specs });
+    const car = await carDB.getCarById(id);
+    const specs = await carDB.getCarSpecsByCarId(id);
+
+    res.status(200).render("car.ejs", { title: car.name, car, specs });
+  },
+];
+
+function carNewGet(req, res) {
+  console.log("newget");
+  res.status(200).render("cars_new.ejs", { title: "New car" });
 }
 
-async function carPost(req, res) {
-  throw error(;);
-}
+const carNewPost = [
+  carValidations,
+  async function carNewPost(req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res
+        .status(400)
+        .render("cars_new.ejs", { title: "New car", errors: errors.array() });
+    }
+
+    const newCar = {
+      category_id: req.body("category-id"),
+      brand_id: req.body("brand-id"),
+      name: req.body("name"),
+      model_year: req.body("model-year"),
+      price: req.body("price"),
+      horsepower: req.body("horsepower"),
+      torque: req.body("torque"),
+      top_speed: req.body("top-speed"),
+      acceleration_0_60: req.body("acceleration-0-60"),
+      transmission: req.body("transmission"),
+      drivetrain: req.body("drivetrain"),
+      fuel_type: req.body("fuel-type"),
+      engine_size: req.body("engine-size"),
+      seats: req.body("seats"),
+      color: req.body("color"),
+      in_stock: req.body("in-stock"),
+      stock_quantity: req.body("stock-quantity"),
+    };
+
+    const newId = await carDB.createCar(newCar);
+
+    res.redirect(`/cars/${newId}`);
+  },
+];
+
+module.exports = {
+  carsGet,
+  carGet,
+  carNewGet,
+  carNewPost,
+};
