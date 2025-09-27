9f3cf22c694c0d74c602b84ea552496fcb4c50dc
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-09-27 20:41:54 -0300

    Create field validators file

---
+++ controllers/validators.js
@@ -0,0 +1,45 @@
+const { body } = require("express-validator");
+
+const ERRORS = {
+  INT: "must be integer",
+  LENGTH: (maxLength) =>
+    `must be at least 1 character and at most ${maxLength} characters`,
+  MAX: (max) => `must ve less tha ${max}`,
+  DECIMAL: (digits) => `must have ${digits} digits or none`,
+};
+
+function toCapitalized(string) {
+  return `${string.slice(0, 1).toUpperCase()}${string.slice(1).toLowerCase()}`;
+}
+
+function createIntChain(field, max = Number.MAX_SAFE_INTEGER) {
+  const casedField = toCapitalized(field);
+  return body(field)
+    .isInt()
+    .withMessage(`${casedField} ${ERRORS.INT}`)
+    .isInt({ min: 0, max: max })
+    .withMessage(`${casedField} ${ERRORS.MAX(max)}`);
+}
+
+function createTextChain(field, maxLength) {
+  const casedField = toCapitalized(field);
+  return body(field)
+    .trim()
+    .isLength({ min: 1, max: maxLength })
+    .withMessage(`${casedField} ${ERRORS.LENGTH(maxLength)}`);
+}
+
+function createDecimalChain(field, max, digits) {
+  const casedField = toCapitalized(field);
+  return body(field)
+    .isFloat({ lt: max })
+    .withMessage(`${casedField} ${ERRORS.MAX(max)}`)
+    .isDecimal({ force_decimal: false, decimal_digits: digits })
+    .withMessage(`${casedField} ${ERRORS.DECIMAL(digits)}`);
+}
+
+module.exports = {
+  createIntChain,
+  createDecimalChain,
+  createTextChain,
+};
