c65d33bcf0d7f935eeb608f03a5c7f670dfc6094
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-09-28 20:56:59 -0300

    Return the result of excecuting the query
    Used when new car is inserted and the new
    id is returned

--- db/queries.js
+++ db/queries.js
@@ -2,7 +2,7 @@ const pool = require("./pool");
 
 async function runQuery(query, params = []) {
   try {
-    await pool.query(query, params);
+    return await pool.query(query, params);
   } catch (error) {
     console.error(error);
     throw error;
