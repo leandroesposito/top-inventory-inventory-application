36a3acbdf9398a795f3ff06fa907beb32716af05
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-10-04 18:57:19 -0300

    Create brands controller

---
+++ controllers/brads.js
@@ -0,0 +1,107 @@
+const { param, validationResult } = require("express-validator");
+const brandsDB = require("../db/brands");
+const carsDB = require("../db/cars");
+const { createTextChain, createIntChain } = require("./validators");
+
+const validateBrand = [
+  createTextChain("name", 100),
+  createTextChain("country", 40),
+  createIntChain("founded-year"),
+  createTextChain("description"),
+];
+
+const brandExist = () =>
+  param("id").custom(async (value, { req }) => {
+    const brand = await brandsDB.getBrandById(value);
+    if (!brand) {
+      throw new Error(`Brand with id ${value} doesn't exist`);
+    }
+    req.locals = { brand };
+  });
+
+async function brandsGet(req, res) {
+  const brands = await brandsDB.getAllBrands();
+
+  res.status(200).render("brands.ejs", { title: "Brands", brands });
+}
+
+const brandGet = [
+  param("id").isInt().withMessage("Id parameter must be a number"),
+  brandExist(),
+  async function brandGet(req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res
+        .status(400)
+        .render("brand.ejs", { title: "Brand Error", errors: errors.array() });
+    }
+
+    const brand = req.locals.brand;
+    const cars = await carsDB.getCarsByBrandId(brand.id);
+
+    res.status(200).render("brand.ejs", { title: brand.name, brand, cars });
+  },
+];
+
+async function brandFormGet(req, res) {
+  const title = res.locals.brand ? "Edit brand" : "New brand";
+
+  res.status(200).render("brands_form.ejs", {
+    title: title,
+    errors: res.locals.errors,
+    brand: res.locals.brand,
+  });
+}
+
+const brandFormPost = [
+  validateBrand,
+  async function brandFormPost(req, res) {
+    const brand = {
+      id: req.body.id,
+      name: req.body.name,
+      country: req.body.country,
+      founded_year: req.body["founded-year"],
+      description: req.body.description,
+    };
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.locals.brand = brand;
+      res.locals.errors = errors.array();
+
+      return brandFormGet(req, res);
+    }
+
+    if (brand.id) {
+      await brandsDB.updateBrand(brand.id, brand);
+    } else {
+      brand.id = await brandsDB.createBrand(brand);
+    }
+
+    res.redirect(`/brands/view/${brand.id}`);
+  },
+];
+
+const brandEdit = [
+  param("id").isInt().withMessage("Id parameter must be a number"),
+  brandExist(),
+  async function brandEdit(req, res) {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.locals.errors = errors.array();
+    } else {
+      res.locals.brand = req.locals.brand;
+    }
+
+    brandFormGet(req, res);
+  },
+];
+
+module.exports = {
+  brandsGet,
+  brandGet,
+  brandFormGet,
+  brandFormPost,
+  brandEdit,
+};
