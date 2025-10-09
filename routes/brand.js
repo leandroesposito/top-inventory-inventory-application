48529b13d654740c19d14072f562d38c2afaab78
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-10-09 18:12:22 -0300

    Add delete routes

--- routes/brand.js
+++ routes/brand.js
@@ -8,5 +8,7 @@ brandRouter.get("/new", brandsController.brandFormGet);
 brandRouter.post("/new", brandsController.brandFormPost);
 brandRouter.get("/edit/:id", brandsController.brandEdit);
 brandRouter.get("/view/:id", brandsController.brandGet);
+brandRouter.get("/delete/:id", brandsController.brandDeleteGet);
+brandRouter.post("/delete/:id", brandsController.brandDeletePost);
 
 module.exports = brandRouter;
